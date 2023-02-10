04aabae47f333fafd0f718b479df1e6e806fa148
necessu <necessus@gmail.com>
2023-02-10 18:36:01 +0100

    my answer

--- reports.py
+++ reports.py
@@ -1,10 +1,20 @@
+import datetime
+import time
+
+HOTEL = 0
+DATE = 1
+CHILDREN = 3
+RESERVATION_STATUS = 8
+
+
 def import_data(filename='booking.txt'):
     """
     Import data from a file to a list. The columns are marked as follows:
     hotel;arrival_date;booked_nights;adults;children;babies;meal;country;reservation_status;reservation_status_date
     Expected returned data format:
         [["Resort Hotel", "01/22/2017", 4, 2, 0, 0, "YES", "PL", "Check-Out", "09/20/2022"],
-        ["City Hotel", "01/22/2017", 2, 2, 0, 0, "NO", "FR", "Cancelled", "09/20/2022"],
+        ["City Hotel", "01/22/2017", 2, 2, 0, 0,
+            "NO", "FR", "Cancelled", "09/20/2022"],
         ...]
 
     :param str filename: optional, name of the file to be imported
@@ -12,7 +22,12 @@ def import_data(filename='booking.txt'):
     :returns: list of lists representing accomodation booking data
     :rtype: list
     """
-    pass
+    with open(filename, "r") as file:
+        bookings = []
+        for line in file:
+            booking = line.strip().split(";")
+            bookings.append(booking)
+        return bookings
 
 
 def export_data(bookings, filename='booking.txt', mode='a'):
@@ -28,7 +43,17 @@ def export_data(bookings, filename='booking.txt', mode='a'):
     :raises ValueError: if mode other than 'w' or 'a' was given. Error message:
         'Wrong write mode'
     """
-    pass
+    modes = ["a", "w"]
+    if mode in modes:
+        text_file = open(filename, mode)
+        for line in range(0, len(bookings)):
+            row = ";".join(bookings[line])
+            if line == len(bookings) - 1:
+                text_file.writelines(row)
+            else:
+                text_file.writelines(row + "\n")
+    else:
+        raise ValueError('Wrong write mode')
 
 
 def get_rows_by_booking_status(rows, status):
@@ -42,7 +67,17 @@ def get_rows_by_booking_status(rows, status):
     :returns: all rows of given status
     :rtype: list
     """
-    pass
+    status_list = set([])
+    for booking in rows:
+        if booking[RESERVATION_STATUS] not in status_list:
+            status_list.add(booking[RESERVATION_STATUS])
+    if status not in status_list:
+        raise ValueError('Status is not present in list')
+    filtered_status = []
+    for booking in rows:
+        if booking[RESERVATION_STATUS] == status:
+            filtered_status.append(booking)
+    return filtered_status
 
 
 def get_rows_by_date(rows, date_in, date_out):
@@ -53,6 +88,23 @@ def get_rows_by_date(rows, date_in, date_out):
     :returns: list of booking in date range betwee date_in and date_out
     :rtype: list
     """
+    date_in = convert_to_timestamp(date_in)
+    date_out = convert_to_timestamp(date_out)
+    for booking in rows:
+        if date_in > convert_to_timestamp(booking[DATE]):
+            pass
+
+
+def convert_to_timestamp(date_str):
+    date_list = date_str.strip().split("/")
+    month, day, year = date_list
+    date_t = datetime.datetime(int(year), int(month), int(day))
+    date = time.mktime(date_t.timetuple())
+    # date = datetime.timestamp(date_t)
+    return date
+
+
+def get_date_out():
     pass
 
 
@@ -64,7 +116,12 @@ def children_number_in_date(rows, date, hotel):
     :returns: number of chidren
     :rtype: int
     """
-    pass
+    children_amount = 0
+    for booking in rows:
+        if booking[DATE] == date and booking[HOTEL] == hotel:
+            print(int(booking[CHILDREN]))
+            children_amount += int(booking[CHILDREN])
+    return children_amount
 
 
 def display_reservation(rows, date):
@@ -77,4 +134,11 @@ def display_reservation(rows, date):
 
     Please get check out date based on arrival_date and booked nights
     """
-    pass
+    for booking in rows:
+        pass
+
+
+# export_data(import_data(), "new_booking.txt", "q")
+# print(get_rows_by_booking_status(import_data(), "CheOut"))
+# print(children_number_in_date(import_data(), "09/18/2022", "Resort Hotel"))
+# print(convert_to_timestamp("01/22/2017"))
